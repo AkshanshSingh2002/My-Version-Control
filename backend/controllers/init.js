795d7dd582c47b0e6902ec2eb67cec5b1698ee97
Akshansh Singh <akshanshs2002@gmail.com>
2025-07-12 21:52:23 +0530

    added .gitignore and working on controllers/init.js file

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,19 @@
+import fs from "fs/promises";
+import path from "path";
+
+
 export async function initRepo() {
 	console.log("init command called");
+
+    const repoPath = path.resolve(process.cwd(), ".myGit");
+    const commitsPath = path.join(repoPath, "commits");
+
+    try {
+        
+    } catch (err) {
+        console.error("Error initialising repository", err);
+
+    }
 }
 
 
