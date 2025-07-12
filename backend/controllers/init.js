a5aed58d8c2510893c6363857813bf6c8aaf1328
Akshansh Singh <akshanshs2002@gmail.com>
2025-07-12 22:06:43 +0530

    completed controllers/init.js and Started working on add.js

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -9,7 +9,13 @@ export async function initRepo() {
     const commitsPath = path.join(repoPath, "commits");
 
     try {
-        
+        await fs.mkdir(repoPath, { recursive: true});
+        await fs.mkdir(commitsPath, { recursive: true});
+        await fs.writeFile(
+            path.join(repoPath, "config.json"),
+            JSON.stringify({bucket: process.env.S3_BUCKET})
+        );
+        console.log("Repository initialised!")
     } catch (err) {
         console.error("Error initialising repository", err);
 
