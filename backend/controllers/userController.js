1e3a28b7ce6035cf83d3b9f6a2dbe98933a1b27f
Akshansh Singh <akshanshs2002@gmail.com>
2025-07-19 11:21:55 +0530

    working on repo controller logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
-import { MongoClient } from "mongodb";
-import User from "../models/userModel.js";
+import { MongoClient, ReturnDocument } from "mongodb";
 import "dotenv/config";
+import { ObjectId } from "mongodb";
 
 const mongodbURI = process.env.MONGODB_URI;
 
@@ -17,10 +17,6 @@ async function connectClient() {
 		await client.connect();
 	}
 }
-
-export const getAllUsers = (req, res) => {
-	res.send("All user fetched!");
-};
 
 export const signup = async (req, res) => {
 	const { username, password, email } = req.body;
@@ -37,27 +33,30 @@ export const signup = async (req, res) => {
 		const salt = await bcrypt.genSalt(10);
 		const hashedPassword = await bcrypt.hash(password, salt);
 
-        const newUser = {
-            username,
-            password: hashedPassword,
-            email,
-            repositories : [],
-            followedUsers : [],
-            starRepos : [],
-        };
+		const newUser = {
+			username,
+			password: hashedPassword,
+			email,
+			repositories: [],
+			followedUsers: [],
+			starRepos: [],
+		};
 
-        const result = await usersCollection.insertOne(newUser);
-        const token = jwt.sign({id: result.insertedId}, process.env.JWT_SECRET_KEY, {expiresIn: "1h"});  
-        res.json({token});  
-
+		const result = await usersCollection.insertOne(newUser);
+		const token = jwt.sign(
+			{ id: result.insertedId },
+			process.env.JWT_SECRET_KEY,
+			{ expiresIn: "1h" }
+		);
+		res.json({ token });
 	} catch (error) {
 		console.error("Error during signup:", error.message);
-        res.status(500).send("Server error during signup.");
+		res.status(500).send("Server error during signup.");
 	}
 };
 
 export async function login(req, res) {
-	const { email, password} = req.body;
+	const { email, password } = req.body;
 	try {
 		await connectClient();
 		const db = client.db("githubclone");
@@ -73,22 +72,102 @@ export async function login(req, res) {
 			return res.status(400).json({ message: "Invalid Credentials!" });
 		}
 
-		const token = jwt.sign({id: user._id}, process.env.JWT_SECRET_KEY, {expiresIn: "1h"});
+		const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+			expiresIn: "1h",
+		});
 		res.json({ token, userId: user._id });
 	} catch (error) {
 		console.error("Error during login: ", error.message);
 		res.status(500).send("Server error during login.");
 	}
-};
+}
 
-export const getUsersProfile = (req, res) => {
-	res.send("Profile fetched!");
-};
+export async function getAllUsers(req, res) {
+	try {
+		await connectClient();
+		const db = client.db("githubclone");
+		const usersCollection = db.collection("users");
 
-export const updateUsersProfile = (req, res) => {
-	res.send("Profile Updated!");
-};
+		const users = await usersCollection.find({}).toArray();
+		res.json(users);
+	} catch (error) {
+		console.error("Error during fetching all users: ", error.message);
+		res.status(500).send("Server error during fetching all users!");
+	}
+}
 
-export const deleteUsersProfile = (req, res) => {
-	res.send("Profile deleted!");
-};
+export async function getUsersProfile(req, res) {
+	const currentID = req.params.id;
+
+	try {
+		await connectClient();
+		const db = client.db("githubclone");
+		const usersCollection = db.collection("users");
+
+		const user = await usersCollection.findOne({
+			_id: new ObjectId(currentID),
+		});
+		if (!user) {
+			return res.status(404).json({ message: "User not Found!" });
+		}
+
+		res.send(user);
+	} catch (err) {
+		console.error("Error during fetching all users: ", err.message);
+		res.status(500).send("Server error during fetching all users!");
+	}
+}
+
+export async function updateUsersProfile(req, res) {   // not getting correct output
+	const currentID = req.params.id;
+	const { email, password } = req.body;
+
+	try {
+		await connectClient();
+		const db = client.db("githubclone");
+		const usersCollection = db.collection("users");
+
+		let updateFields = { email };
+		if (password) {
+			const salt = await bcrypt.genSalt(10);
+			const hashedPassword = await bcrypt.hash(password, salt);
+			updateFields.password = hashedPassword;
+		}
+
+		const result = await usersCollection.findOneAndUpdate(
+			{ _id: new ObjectId(currentID) },
+			{ $set: updateFields },
+			{ returnDocument: "after" }
+		);
+		if (!result.value) {
+			return res.status(404).json({ message: "User not found!"});
+		}
+
+		res.send(result.value);
+
+	} catch (err) {
+		console.error("Error during updating: ", err.message);
+		res.status(500).send("Server error during fetching all users!");
+	}
+}
+
+export async function deleteUsersProfile(req, res) {
+	const currentID = req.params.id;
+
+	try {
+		await connectClient();
+		const db = client.db("githubclone");
+		const usersCollection = db.collection("users");
+
+		const result = await usersCollection.deleteOne({ _id: new ObjectId(currentID)});
+
+		if (!result.deleteCount == 0) {
+			return res.status(404).json({ message: "User not found!"});
+		}
+
+		res.json({ message: "User deleted successfully!"});
+	} catch (error) {
+		console.error("Error during deleting user:", error.message);
+		res.status(500).send("Server error during deleting user. ");
+	}
+}
--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -3,11 +3,11 @@ import * as userController from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
-userRouter.get("/allUsers", userController.getAllUsers); 
 userRouter.post("/signup", userController.signup); 
 userRouter.post("/login", userController.login); 
-userRouter.get("/getProfile", userController.getUsersProfile); 
-userRouter.put("/updateProfile", userController.updateUsersProfile); 
-userRouter.delete("/deleteProfile", userController.deleteUsersProfile); 
+userRouter.get("/allUsers", userController.getAllUsers); 
+userRouter.get("/userProfile/:id", userController.getUsersProfile); 
+userRouter.put("/updateProfile/:id", userController.updateUsersProfile); 
+userRouter.delete("/deleteProfile/:id", userController.deleteUsersProfile); 
 
 export default userRouter;
